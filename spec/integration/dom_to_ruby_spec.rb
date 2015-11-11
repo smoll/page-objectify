97750382974f78ed8210f223a529862054a77597
Shujon Mollah <mollah@gmail.com>
2015-11-11 14:31:55 -0500

    Write failing spec for #17

--- spec/integration/dom_to_ruby_spec.rb
+++ spec/integration/dom_to_ruby_spec.rb
@@ -8,13 +8,35 @@ RSpec.describe PageObjectify::DOMToRuby do
   let(:config) { PageObjectify::Config.new(generator_class: "StubbedPageGenerator") }
 
   describe "#unparse" do
-    it "returns the expected Ruby source as a String" do
-      expected_source = <<-RUBY.gsub(/^\s*\|/, '')
-                        |class StubbedPage < BasePage
-                        |  link(:myid, id: "myid")
-                        |end
-                        RUBY
-      expect(subject.unparse).to eq(expected_source.chomp)
+    context "page with 1 element" do
+      it "returns the expected Ruby source as a String" do
+        expected_source = <<-RUBY.gsub(/^\s*\|/, '')
+                          |class StubbedPage < BasePage
+                          |  link(:myid, id: "myid")
+                          |end
+                          RUBY
+        expect(subject.unparse).to eq(expected_source.chomp)
+      end
+    end
+
+    context "page with 3 elements" do
+      let(:dom) { PageObjectify::DOM.new(html) }
+      let(:html) do
+        '<a href="1.html" id="c">something</a>'\
+        '<a href="2.html" id="b">something</a>'\
+        '<a href="3.html" id="a">something</a>'
+      end
+
+      it "lists all 3 accessors, sorted lexicographically on method name" do
+        expected_source = <<-RUBY.gsub(/^\s*\|/, '')
+                          |class StubbedPage < BasePage
+                          |  link(:a, id: "a")
+                          |  link(:b, id: "b")
+                          |  link(:c, id: "c")
+                          |end
+                          RUBY
+        expect(subject.unparse).to eq(expected_source.chomp)
+      end
     end
   end
 end
