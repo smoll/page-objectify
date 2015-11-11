b062bb3d75a40f41af902958703caf896e7a4aa7
Shujon Mollah <mollah@gmail.com>
2015-11-11 01:49:55 -0500

    Write test to catch invalid generated PO accessor methods

--- spec/lib/page-objectify/dom_spec.rb
+++ spec/lib/page-objectify/dom_spec.rb
@@ -1,9 +1,32 @@
+require "page-object"
 require "page-objectify/dom"
 require "page-objectify/logging"
 require "spec_helper"
 
 RSpec.describe PageObjectify::DOM do
   subject { described_class.new(html) }
+  let(:html) { '<html></html>' }
+
+  # For these 2 tests, see `puts` from: #to_accessors -> script tag -> does not attempt to generate an accessor for it
+
+  describe "#tags_to_accessors" do
+    # Compare against PageObject's list of instance methods
+    it "doesn't contain any invalid mappings" do
+      subject.tags_to_accessors.each do |k, v|
+        expect(PageObject::LocatorGenerator::ADVANCED_ELEMENTS).to include(v.to_sym)
+      end
+    end
+  end
+
+  describe "#input_types_to_accessors" do
+    # Compare against PageObject's list of instance methods
+    it "doesn't contain any invalid mappings" do
+      subject.input_types_to_accessors.each do |k, v|
+        expect(PageObject::LocatorGenerator::ADVANCED_ELEMENTS).to include(v.to_sym)
+      end
+    end
+  end
+
   describe "#to_accessors" do
     context "h1 tag" do
       let(:html) { '<h1 id="myid">something</h1>' }
@@ -31,6 +54,9 @@ RSpec.describe PageObjectify::DOM do
 
         expect(subject.to_accessors).to eq []
         expect(captured.string).to_not include "Tag script is not supported!"
+
+        # echo logger output of tag -> accessor method mapping, for debugging purposes
+        puts captured.string
       end
     end
   end
